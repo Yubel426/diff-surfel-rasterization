7abea9357dc8d2847f7b631c15c1c0520d1ec16f
Yubel426 <849066607@qq.com>
2024-05-09 22:01:42 +0800

    add phone depth

--- cuda_rasterizer/forward.cu
+++ cuda_rasterizer/forward.cu
@@ -355,10 +355,9 @@ renderCUDA(
 
 			// compute intersection and depth
 			float rho = min(rho3d, rho2d);
-			float depth = (rho3d <= rho2d) ? (s.x * Tw.x + s.y * Tw.y) + Tw.z : Tw.z; 
-			float2 normal_scaling = collected_normal_scaling[j];
-			depth += normal_scaling.x * s.x + normal_scaling.y * s.y;
-			if (depth < near_n) continue;
+			
+			float depth = (rho3d <= rho2d) ? (s.x * Tw.x + s.y * Tw.y) + Tw.z : Tw.z; // splat depth
+			if (depth < NEAR_PLANE) continue;
 			float4 nor_o = collected_normal_opacity[j];
 			float normal[3] = {nor_o.x, nor_o.y, nor_o.z};
 			float opa = nor_o.w;
